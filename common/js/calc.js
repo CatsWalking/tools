f2e96c23fb81063892c2c11793dab7bde5ae76d6
CatsWalking <mami@atmark.gr.jp>
2023-01-13 18:41:29 +0900

    first commit

---
+++ common/js/calc.js
@@ -0,0 +1,137 @@
+/*----------------------------
+Roku tools
+*/
+'use strict';
+
+let i;
+let active = 'prime';
+let active_input = $('#'+active).find('.input');
+let active_result;
+
+/*-----------------------
+  init
+-----------------------*/
+function init(){
+    active_input = $('#'+active).find('.input');
+    active_result = $('#'+active).find('.result');
+    $('.input').removeClass('active');
+    active_input.addClass('active');
+}
+init();
+$('#'+active).find('.input').focus();
+
+$('main').click(function(){
+    // init();
+})
+// focus
+$('#calc').on('focus', '.input', function(){
+    active = $(this).parent().parent().attr('id');
+    init();
+})
+$('#calc').on('click', '.input', function(){
+    $(this).select();
+})
+$('#calc').on('click', 'tr', function(){
+    active = $(this).attr('id');
+    init();
+})
+
+/*-----------------------
+  テンキー
+-----------------------*/
+let num
+$('.tenkey_box li').click(function(){
+    let n = $(this).html();
+    active_input.val(active_input.val()+n);
+    calc(active_input);
+})
+
+/*-----------------------
+  計算
+-----------------------*/
+function calc(elem){
+    let val = elem.val();
+    let id = elem.parent().parent().attr('id');
+    let res;
+    active_result.html('')
+    if(val!='' && val!='-' && val!='0'){
+        val = parseInt(zenToHan(val));
+        switch(id){
+            case 'prime':
+                res = prime(val);
+                break;
+            case 'squre':   
+                // 平方数
+                res = val*val;
+                break;
+            case 'squre_root':
+                // 平方根
+                res = Math.round( Math.sqrt(val) * 1000 ) / 1000;
+                break;
+            case 'pi':
+                // 円周率
+                res = Math.round(val*3.14*100)/100
+                break; 
+            case 'pi2':
+                // 円周面積
+                res = Math.round(val*val*3.14*100)/100
+                break;         
+        }
+        active_result.html(res)
+    }
+}
+$('#calc .input').keyup(function(){
+    calc($(this));
+})
+
+/*-----------------------
+  clear
+-----------------------*/
+$('#js_clear').click(function(){
+    active_input.val('');
+    active_result.html('');
+})
+$('#js_all_clear').click(function(){
+    $('.input').val('');
+    $('.result').html('');
+})
+/*-----------------------
+  素因数分解
+-----------------------*/
+function prime(val){
+    let primes1 = [];
+    let primes2 = [];
+    let primes3 = [];
+    if(Number(val)){
+        for(i=2; i<=50; i++){
+            for(let j=0; j<10; j++){
+                if(val % i ==0){
+                    primes1.push(i);
+                    val = val/i;
+                } else {
+                    break;
+                }
+            }
+        }
+        if(val>1){
+            primes1.push(val);
+        }
+        primes1.forEach(function(i, e){
+            if(primes2[i]){
+                primes2[i]++;
+            } else {
+                primes2[i] = 1;
+            }
+        })
+        primes2.forEach(function(i, e){
+            if(i==1){
+                primes3.push("<div class='prime_number'>"+e+"</div>");
+            } else {
+                primes3.push("<div class='prime_number'>"+e+"</div><div class='ruby'>"+i+"</div>");
+            }
+        })
+        return primes3.join(', ');
+    }
+    return '';
+}
+
---
+++ common/js/tools.js
@@ -0,0 +1,44 @@
+/*----------------------------
+Roku tools
+*/
+'use strict';
+
+// スマホだったらソフトウェアキーボードを表示しない
+if(isSP()){
+    $(".input").attr('readOnly', 'true');
+} else {
+    $(".input").removeAttr('readOnly');
+}
+
+
+/*-----------------------
+  エンターキー
+-----------------------*/
+// $('.input').keypress(function(e) {
+//     // enter, tab
+//     if (e.keyCode == 13 || e.keyCode == 9) {
+//         $(this).select();
+//       return false;
+//     }
+// });
+
+/*-----------------------------
+ * タイマー
+ */
+let startTime = null;   // timer
+let timer_id;
+let isPlay = false;
+let t ;
+function runTimer(){
+    t = ((Date.now() - startTime) / 1000).toFixed(2);
+    document.getElementById('timer').textContent=t;
+    timerid = setTimeout(runTimer, 12);
+}
+
+
+
+$('#js_reset').click(function(){
+  openConfirm('リセットしますか？', function(){
+    location.href=location.href;
+  })
+})
